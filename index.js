930b1411648853446f225e6198ced9dccc9696ad
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-12 04:28:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ function destructivelyRemoveLastKitten () {
   kittens.pop()
   return kittens
 }
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name]
+  return moreKittens
+}
+
+function prependKitten(name){
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
+
